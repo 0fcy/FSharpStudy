ec968f6f723b7b5a60d8874c16fbf33650f79942
SzymonFrac <szymon.frac195@gmail.com>
2026-08-24 15:56:50 +0100

    Print sequence with Seq.iter

--- src/FSharpStudy.Sequences/Program.fs
+++ src/FSharpStudy.Sequences/Program.fs
@@ -1,6 +1,12 @@
 ﻿
 let mySequence = seq { 1 .. 3 .. 10 }
 
+// Print sequence with for loop
 for i in mySequence do
     printfn "%d" i
 
+printfn ""
+
+// Print sequence with Seq.iter
+mySequence
+    |> Seq.iter (printfn "%d")
